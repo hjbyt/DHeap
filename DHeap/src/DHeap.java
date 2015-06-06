d6fb08dbd9b2f404843c21c40b9ed3f35643c3de
hjbyt <sopu00@gmail.com>
2015-06-06 21:12:18 +0300

    formatted files

--- DHeap/src/DHeap.java
+++ DHeap/src/DHeap.java
@@ -2,141 +2,137 @@
  * D-Heap
  */
 
-public class DHeap
-{
-	
+public class DHeap {
+
     private int size, max_size, d;
     private DHeap_Item[] array;
 
-	// Constructor
-	// m_d >= 2, m_size > 0
-       DHeap(int m_d, int m_size) {
-               max_size = m_size;
-			   d = m_d;
-               array = new DHeap_Item[max_size];
-               size = 0;
-       }
-	
-	// Getter for size
-	public int getSize() {
-		return size;
-	}
-	
-  /**
+    // Constructor
+    // m_d >= 2, m_size > 0
+    DHeap(int m_d, int m_size) {
+        max_size = m_size;
+        d = m_d;
+        array = new DHeap_Item[max_size];
+        size = 0;
+    }
+
+    // Getter for size
+    public int getSize() {
+        return size;
+    }
+
+    /**
      * public void arrayToHeap()
-     *
+     * <p>
      * The function builds a new heap from the given array.
      * Previous data of the heap should be erased.
      * preconidtion: array1.length() <= max_size
-     *   postcondition: isHeap()
-     *                            size = array.length()
+     * postcondition: isHeap()
+     * size = array.length()
      */
-    public void arrayToHeap(DHeap_Item[] array1) 
-    {
-        return; // just for illustration - should be replaced by student code
+    public void arrayToHeap(DHeap_Item[] array1) {
+        //TODO
     }
 
     /**
      * public boolean isHeap()
-     *
+     * <p>
      * The function returns true if and only if the D-ary tree rooted at array[0]
      * satisfies the heap property or size == 0.
-     *   
      */
-    public boolean isHeap() 
-    {
-        return false; // just for illustration - should be replaced by student code
+    public boolean isHeap() {
+        return false; //TODO
     }
 
 
- /**
+    /**
      * public static int parent(i), child(i,k)
      * (2 methods)
-     *
+     * <p>
      * precondition: i >= 0
-     *
-     * The methods compute the index of the parent and the k-th child of 
+     * <p>
+     * The methods compute the index of the parent and the k-th child of
      * vertex i in a complete D-ary tree stored in an array. 1 <= k <= d.
      * Note that indices of arrays in Java start from 0.
      */
-    public static int parent(int i) { return 999;} // just for illustration - should be replaced by student code
-    public static int child (int i, int k) { return 999;} // just for illustration - should be replaced by student code 
-
-    /**
-    * public void Insert(DHeap_Item item)
-    *
-    * precondition: item != null
-    *               isHeap()
-    *               size < max_size
-    * 
-    * postcondition: isHeap()
-    */
-    public void Insert(DHeap_Item item) 
-    {        
-    	return;// should be replaced by student code
+    public static int parent(int i) {
+        return 999; //TODO
     }
 
- /**
-    * public void Delete_Min()
-    *
-    * precondition: size > 0
-    *               isHeap()
-    * 
-    * postcondition: isHeap()
-    */
-    public void Delete_Min()
-    {
-     	// should be replaced by student code
+    public static int child(int i, int k) {
+        return 999; //TODO
+    }
+
+    /**
+     * public void Insert(DHeap_Item item)
+     * <p>
+     * precondition: item != null
+     * isHeap()
+     * size < max_size
+     * <p>
+     * postcondition: isHeap()
+     */
+    public void Insert(DHeap_Item item) {
+        //TODO
+    }
+
+    /**
+     * public void Delete_Min()
+     * <p>
+     * precondition: size > 0
+     * isHeap()
+     * <p>
+     * postcondition: isHeap()
+     */
+    public void Delete_Min() {
+        //TODO
     }
 
 
     /**
      * public String Get_Min()
-     *
+     * <p>
      * precondition: heapsize > 0
-     *               isHeap()
-     *		size > 0
-     * 
+     * isHeap()
+     * size > 0
+     * <p>
      * postcondition: isHeap()
      */
-    public DHeap_Item Get_Min()
-    {
-	return null;// should be replaced by student code
+    public DHeap_Item Get_Min() {
+        return null; //TODO
     }
-	
-  /**
+
+    /**
      * public void Decrease_Key(DHeap_Item item, int delta)
-     *
+     * <p>
      * precondition: item.pos < size;
-     *               item != null
-     *               isHeap()
-     * 
+     * item != null
+     * isHeap()
+     * <p>
      * postcondition: isHeap()
      */
-    public void Decrease_Key(DHeap_Item item, int delta)
-    {
-	return;// should be replaced by student code
+    public void Decrease_Key(DHeap_Item item, int delta) {
+        //TODO
     }
-	
-	  /**
+
+    /**
      * public void Delete(DHeap_Item item)
-     *
+     * <p>
      * precondition: item.pos < size;
-     *               item != null
-     *               isHeap()
-     * 
+     * item != null
+     * isHeap()
+     * <p>
      * postcondition: isHeap()
      */
-    public void Delete(DHeap_Item item)
-    {
-	return;// should be replaced by student code
+    public void Delete(DHeap_Item item) {
+        //TODO
     }
-	
-	/**
-	Return a sorted array containing the same integers in the input array.
-	Sorting should be done using the DHeap.
-	*/
-	public static int []DHeapSort(int[] array) {
-		return;
-	}
+
+    /**
+     * Return a sorted array containing the same integers in the input array.
+     * Sorting should be done using the DHeap.
+     */
+    public static int[] DHeapSort(int[] array) {
+        return null; //TODO
+    }
 }
--- DHeap/src/DHeap_Item.java
+++ DHeap/src/DHeap_Item.java
@@ -1,39 +1,33 @@
 public class DHeap_Item {
 
-		private String name ;
-		private int  key ;
-		private int  pos ; // Position in the heap (if inserted into a heap.)
+    private String name;
+    private int key;
+    private int pos; // Position in the heap (if inserted into a heap.)
 
-		DHeap_Item(String name1,int key1)
-		{
-			name = name1 ;
-			key  = key1 ;
-			pos  = -1   ;
-		}
+    DHeap_Item(String name1, int key1) {
+        name = name1;
+        key = key1;
+        pos = -1;
+    }
 
-		// Setters and Getters
-		public void setKey(int key1) 
-		{
-			key = key1;
-		}
-		
-		public void setPos(int pos1)
-		{
-			pos = pos1;
-		}
-		
-		public String getName()
-		{
-			return name;
-		}
-		
-		public int getKey()
-		{
-			return key;
-		}
-		
-		public int getPos()
-		{
-			return pos;
-		}
+    // Setters and Getters
+    public void setKey(int key1) {
+        key = key1;
+    }
+
+    public void setPos(int pos1) {
+        pos = pos1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int getPos() {
+        return pos;
+    }
 }
