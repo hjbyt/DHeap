66b9b4ebf9233f64e9b900e693181bb4757034ae
hjbyt <sopu00@gmail.com>
2015-06-06 21:14:12 +0300

    added test class

---
+++ DHeap/src/DHeapTest.java
@@ -0,0 +1,67 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DHeapTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSize() throws Exception {
+
+    }
+
+    @Test
+    public void testArrayToHeap() throws Exception {
+
+    }
+
+    @Test
+    public void testIsHeap() throws Exception {
+
+    }
+
+    @Test
+    public void testParent() throws Exception {
+
+    }
+
+    @Test
+    public void testChild() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete_Min() throws Exception {
+
+    }
+
+    @Test
+    public void testGet_Min() throws Exception {
+
+    }
+
+    @Test
+    public void testDecrease_Key() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testDHeapSort() throws Exception {
+
+    }
+}
